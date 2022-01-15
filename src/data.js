7b7b53391a4b70c4e6b816af079a0e00dd4bce2a
Tasmim-Adib <tasmim.adib@gmail.com>
2022-01-15 08:44:31 +0600

    start here

---
+++ src/data.js
@@ -0,0 +1,73 @@
+const data = {
+    products:[
+        {
+            _id : '1',
+            name : 'Nike Slim Shirt',
+            category : 'Shirt',
+            image : '/images/p1.jpg',
+            price : 120,
+            brand : 'Nike',
+            rating : 4.5,
+            reviews : 10,
+            description : 'Branded Shirt. Very beautiful.'
+        },
+        {
+            _id : '2',
+            name : 'Adidas Slim Shirt',
+            category : 'Shirt',
+            image : '/images/p2.jpg',
+            price : 110,
+            brand : 'Adidas',
+            rating : 4,
+            reviews : 12,
+            description : 'Branded Shirt. Very beautiful.'
+        },
+        {
+            _id : '3',
+            name : 'Bata Slim Shirt',
+            category : 'Shirt',
+            image : '/images/p3.jpg',
+            price : 80,
+            brand : 'Bata',
+            rating : 3.5,
+            reviews : 5,
+            description : 'Branded Shirt. Very beautiful.'
+        },
+        {
+            _id : '4',
+            name : 'Apex Slim Shirt',
+            category : 'Shirt',
+            image : '/images/p4.jpeg',
+            price : 90,
+            brand : 'Apex',
+            rating : 4.5,
+            reviews : 9,
+            description : 'Branded Shirt. Very beautiful.'
+        },
+        {
+            _id : '5',
+            name : 'Gentle Park Slim Shirt',
+            category : 'Shirt',
+            image : '/images/p5.jpeg',
+            price : 100,
+            brand : 'Gentle Park',
+            rating : 4.5,
+            reviews : 10,
+            description : 'Branded Shirt. Very beautiful.'
+        },
+        {
+            _id : '6',
+            name : 'Tanjim Slim Shirt',
+            category : 'Shirt',
+            image : '/images/p6.jpg',
+            price : 90,
+            brand : 'Tanjim',
+            rating : 4.5,
+            reviews : 10,
+            description : 'Branded Shirt. Very beautiful.'
+        }
+        
+    ]
+};
+
+export default data;
